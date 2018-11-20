bc306cb25869a18a982cb2a2a8c0cdfdca499511
Wator Vapor <watorvapor@gmail.com>
2018-11-20 20:40:48 +0900

    Add files via upload

--- leg/leg.ino
+++ leg/leg.ino
@@ -4,10 +4,21 @@
 const static char MOTER_PWM_WHEEL = 12;
 const static char MOTER_CCW_WHEEL = 27;
 const static char MOTER_FGS_WHEEL = 29;
+
+
+
+const static char MOTER_CURRENT_LINEAR = 2;
+const static char MOTER_PWM_LINEAR = 3;
+const static char MOTER_STANDBY_LINEAR = 5;
+const static char MOTER_A1_LINEAR = 7;
+const static char MOTER_A2_LINEAR = 9;
+
+
+
 unsigned char speed_wheel = 0x0;
 
 static long wheelRunCounter = -1;
-static long const iRunTimeoutCounter = 30000L * 1L;
+static long const iRunTimeoutCounter = 30000L * 2L;
 
 
 
@@ -32,7 +43,19 @@ void setup()
   pinMode(MOTER_CCW_WHEEL, OUTPUT);
   pinMode(MOTER_FGS_WHEEL, INPUT);
   
+
+  pinMode(MOTER_CURRENT_LINEAR, INPUT);
+  pinMode(MOTER_PWM_LINEAR, OUTPUT);
+  pinMode(MOTER_STANDBY_LINEAR, OUTPUT);
+  pinMode(MOTER_A1_LINEAR, OUTPUT);
+  pinMode(MOTER_A2_LINEAR, OUTPUT);
   
+
+  digitalWrite(MOTER_PWM_LINEAR, HIGH);
+  digitalWrite(MOTER_STANDBY_LINEAR, HIGH);
+  digitalWrite(MOTER_A1_LINEAR, HIGH);
+  digitalWrite(MOTER_A2_LINEAR, LOW);
+
 
   TCCR1B = (TCCR1B & 0b11111000) | 0x01;
   
@@ -70,6 +93,20 @@ void runWheel(int spd,int front) {
   }
 }
 
+void runLinear(int distance,int ground) {
+  if(ground) {
+    digitalWrite(MOTER_A1_LINEAR, LOW);
+    digitalWrite(MOTER_A2_LINEAR, HIGH);
+    DUMP_VAR(distance);
+  } else {
+    digitalWrite(MOTER_A1_LINEAR, HIGH);
+    digitalWrite(MOTER_A2_LINEAR, LOW);
+    DUMP_VAR(distance);
+  }
+}
+
+
+
 void tryConfirmJson() {
   StaticJsonDocument<256> jsonBuffer;
   auto errorDS = deserializeJson(jsonBuffer,InputCommand);
@@ -82,14 +119,21 @@ void tryConfirmJson() {
   for (auto kv : root) {
     JsonObject params = kv.value();
     String motor = kv.key().c_str();
-    int spd = params["s"];
-    int front = params["f"];
     DUMP_VAR(motor);
-    DUMP_VAR(spd);
-    DUMP_VAR(front);
     //CONFIRM_WHEEL();
-    if(motor == "wheel") {
+    if(motor == "wheel" || motor == "W") {
+      int spd = params["s"];
+      int front = params["f"];
+      DUMP_VAR(spd);
+      DUMP_VAR(front);
       runWheel(spd,front);
+    }
+    if(motor == "linear" || motor == "L") {
+      int distance = params["d"];
+      int ground = params["g"];
+      DUMP_VAR(distance);
+      DUMP_VAR(ground);
+      runLinear(distance,ground);
     }
   }
 }
@@ -123,6 +167,17 @@ void run_comand() {
   }
 }
 
+
+void readStatus() {
+  int current = analogRead(MOTER_CURRENT_LINEAR);
+  if(abs(current - 506) > 10) {
+    Serial.print("current=<");
+    Serial.print(current);
+    Serial.println(">");
+  }
+}
+
+
 void loop() {
   // stop
   if(wheelRunCounter-- <= 0) {
@@ -144,5 +199,6 @@ void loop() {
       InputCommand = "";
     }
   }
+  readStatus();
 }
 
