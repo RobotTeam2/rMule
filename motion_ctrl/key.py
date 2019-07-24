f7da30502ec20ef35a37321d23b5bd40bd497731
Youtaro Mine <youtaro.mine@gmail.com>
2019-07-25 08:36:21 +0900

    change library for key input from pynput
    change to use getch() from
    http://code.activestate.com/recipes/134892/

--- motion_ctrl/key.py
+++ motion_ctrl/key.py
@@ -1,81 +1,100 @@
-from pynput.keyboard import Key, KeyCode, Listener
-import queue
+import time
 
 key_command_map = {
-    Key.tab:["command",["left"]],
-    KeyCode.from_char('/'):["command",["right"]],
-    KeyCode.from_char('*'):["scenario",["walk"]],
-    Key.backspace:["scenario",["back"]],
+    b'\t':["command",["left"]],
+    b'/':["command",["right"]],
+    b'*':["scenario",["walk"]],
+    b'\x08':["scenario",["back"]],
 
-    KeyCode.from_char('7'):["motor_command",["up"]],
-    KeyCode.from_char('8'):["motor_id",["0"]],
-    KeyCode.from_char('9'):["motor_id",["3"]],
-    KeyCode.from_char('-'):["motor_command",["move", 0]],
+    b'7':["motor_command",["up"]],
+    b'8':["motor_id",["0"]],
+    b'9':["motor_id",["3"]],
+    b'-':["motor_command",["move", 0]],
 
-    KeyCode.from_char('4'):["motor_command",["down"]],
-    KeyCode.from_char('5'):["motor_id",["1"]],
-    KeyCode.from_char('6'):["motor_id",["4"]],
-    KeyCode.from_char('+'):["motor_command",["move",100]],
+    b'4':["motor_command",["down"]],
+    b'5':["motor_id",["1"]],
+    b'6':["motor_id",["4"]],
+    b'+':["motor_command",["move",100]],
     
-    KeyCode.from_char('1'):["motor_id",["all"]],  
-    KeyCode.from_char('2'):["motor_id",["2"]],      
-    KeyCode.from_char('3'):["motor_id",["5"]],     
-    Key.enter:["command",["stop"]],
+    b'1':["command",["init"]],  
+    b'2':["motor_id",["2"]],      
+    b'3':["motor_id",["5"]],     
+    b'\r':["command",["stop"]],
 
-    KeyCode.from_char('0'):["command",["init"]], 
-    KeyCode.from_char('.'):["command",["reset"]]
+#    b'0':["command",["reset"]], 
+    b'.':["motor_id",["all"]]
+
 }
 
-def on_press(key):
-    command_queue.put(key)
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
 
-def on_release(key):
-    print('{0} release'.format(key))
-    if key == Key.esc:
-        # Stop listener
-        return False
+    def __call__(self): return self.impl()
 
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
 
 # Collect events until released
 if __name__ == '__main__':
-    command_queue = queue.Queue()
 
-    with Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-
-        print("start listener")
-        motor_command = ""
-        while True:
-            key = command_queue.get()
-            print('{0} pressed'.format(key))
-
-            if key in key_command_map:
-                command = key_command_map[key]
-                if command[0] == "scenario":
-                    print("scenario {}".format(command[1]))
-                elif command[0] == "command":
-                    print("command {}".format(command[1]))
-                elif command[0] == "motor_command":
-                    print("motor_command {}".format(command[1]))
-                    motor_command = command[1]
-                    print("wait motor_id")
-                elif command[0] == "motor_id":
-                    print("motor_id {}".format(command[1]))
-                    
-                    if len(motor_command) > 0:
-                        print("command {} {}".format(motor_command,command[1]))
-                        motor_command = ""
-                    else:
-                        print("motor_command missing")
-
-                else:
-                    pass               
-            
-            if key == Key.esc:
-                break 
-
-        listener.join()
-
+    print("start listener")
+    motor_id = ""
+    getch = _Getch()
     
-    
+    while True:
+        key = getch()
+        print('{0} pressed'.format(key))
+
+        if key == b'\x03':
+            break
+
+        if key in key_command_map:
+            command = key_command_map[key]
+            if command[0] == "scenario":
+                print("scenario {}".format(command[1]))
+                motor_id = ""
+            elif command[0] == "command":
+                print("command {}".format(command[1]))
+                motor_id = ""
+            elif command[0] == "motor_command":
+                print("motor_command {}".format(command[1]))
+                motor_command = command[1]
+                if len(motor_id) > 0:
+                    print("execute motor command {}:{} ".format(command[1],motor_id))
+                    motor_id = ""
+                    time.sleep(1)
+            elif command[0] == "motor_id":
+                print("motor_id {}".format(command[1]))
+                motor_id = command[1]
+                
+            else:
+                pass               
+        
